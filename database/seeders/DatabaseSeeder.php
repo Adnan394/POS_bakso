c600bb516713ebdaa3a5fe8e35ec3dd0e9374381
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-12-24 15:42:23 +0700

    make login system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -22,9 +23,17 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         User::create([
-            'name' => 'Super Admin',
+            'name' => 'Admin Pusat',
             'email' => 'superadmin@gmail.com',
+            'role_id' => '1',
             'password' => Hash::make('superadmin')
+        ]);
+
+        Role::create([
+            'name' => 'SuperAdmin',
+        ]);
+        Role::create([
+            'name' => 'Admin',
         ]);
     }
 }
