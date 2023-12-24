9295e87d55a8c53da63125f7a845d0f04bb52eb3
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-12-24 15:42:23 +0700

    make login system

---
+++ app/Http/Controllers/auth/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
+{
+    public function index() {
+        return view('auth.login');
+    }
+
+    public function store(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            if (Auth::user()->role_id == '1') {
+                return redirect('/superadmin');
+            }
+            if (Auth::user()->role_id == '2') {
+                return redirect('/admin');
+            }
+        }
+
+        return redirect()->route('login')->with('login', 'Email atau password salah.');
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        
+        Auth::logout();
+
+        return redirect('login');
+    }
+
+    public function change(Request $request) {
+        if ($request->newpassword != $request->newpassword_confirmation) {
+            return redirect()->back()->with('login', 'Konfirmasi Password Salah!');
+        }
+
+       if (User::where('id', $request->id)->update(['password' => Hash::make($request->newpassword)])) {
+        return redirect()->back();
+       };
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', [LoginController::class, 'index'])->name('login'); // Mengarahkan ke halaman login
+Route::prefix('/superadmin')->middleware('auth')->group(function() {
+    Route::get('/', function () {
+        return view('superadmin.dashboard');
+    });
+
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
