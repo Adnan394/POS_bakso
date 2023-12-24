83c504ee876c1fa8f9399e405be2c36d72926c11
adnan <adnanega82@gmail.com>
2023-12-24 14:49:22 +0700

    [api] login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('auth')->group(function() {
+    Route::post('login', function(Request $request) {
+        if(Auth::attempt($request->all())) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('login')->plainTextToken;
+
+            return response()->json([
+                'code' => 200, 
+                'message' => "OK",
+                'data' => [
+                    'token' => $token,
+                ]
+            ], 200);
+        }else {
+            return response()->json([
+                'code' => 500, 
+                'message' => 'Internal Server Error',
+                'message' => 'Invalid Username and Password'
+            ]);
+        }
+    });
 });
