e6f3df885d2c252bf458cdfaf5ac3dabc9096917
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2024-01-02 19:30:21 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -251,31 +251,23 @@
                                     aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
                                         class="hide-menu">Dashboard</span></a></li>
                             <li class="list-divider"></li>
-                            <li class="nav-small-cap"><span class="hide-menu">Data Master</span></li>
+                            <li class="nav-small-cap"><span class="hide-menu">Pemesanan</span></li>
 
                             <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
                                     aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
-                                        class="hide-menu">Data Master </span></a>
+                                        class="hide-menu">Transaksi </span></a>
                                 <ul aria-expanded="false" class="collapse  first-level base-level-line">
                                     <li class="sidebar-item"><a href="{{ route('products.index') }}"
-                                            class="sidebar-link"><span class="hide-menu"> Pemesanan
+                                            class="sidebar-link"><span class="hide-menu"> Transaksi Berjalan
                                             </span></a>
                                     </li>
                                     <li class="sidebar-item"><a href="{{ route('outlets.index') }}"
-                                            class="sidebar-link"><span class="hide-menu"> Outlet
-                                            </span></a>
-                                    </li>
-                                    <li class="sidebar-item"><a href="{{ route('payments.index') }}"
-                                            class="sidebar-link"><span class="hide-menu"> Payment Method
+                                            class="sidebar-link"><span class="hide-menu"> Transaksi Selesai
                                             </span></a>
                                     </li>
                                 </ul>
                             </li>
                             <li class="list-divider"></li>
-                            <li class="nav-small-cap"><span class="hide-menu">Extra</span></li>
-                            <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="docs/docs.html"
-                                    aria-expanded="false"><i data-feather="edit-3" class="feather-icon"></i><span
-                                        class="hide-menu">Documentation</span></a></li>
                             <li class="sidebar-item"> <a class="sidebar-link sidebar-link"
                                     href="{{ route('logout') }}" aria-expanded="false"><i data-feather="log-out"
                                         class="feather-icon"></i><span class="hide-menu">Logout</span></a></li>
