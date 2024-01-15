8acc116c34a8f6e93c3dafd9cd6e0721e25ef808
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2024-01-15 23:53:44 +0700

    fiixng something in kasir

--- resources/views/kasir/transaksi/edit.blade.php
+++ resources/views/kasir/transaksi/edit.blade.php
@@ -21,8 +21,9 @@
                                     <th scope="row">{{ $key + 1 }}</th>
                                     <td>{{ \App\Models\Produk::find($product->product_id)->name }}</td>
                                     <td>{{ $product->price }}</td>
-                                    <td><input type="number" name="product[]" value="{{ $product->qty }}" onchange="updateSubtotal(this)"
-                                            data-price="{{ $product->price }}" readonly></td>
+                                    <td><input type="number" name="product[]" value="{{ $product->qty }}"
+                                            onchange="updateSubtotal(this)" data-price="{{ $product->price }}" readonly>
+                                    </td>
                                     <td class="subtotal">Rp. 0</td>
                                 </tr>
                             @endforeach
@@ -34,65 +35,66 @@
                 <form action="{{ route('selesaikan_pesanan') }}" method="POST">
                     @csrf
                     <input type="hidden" name="transaction_id" value="{{ $data->id }}">
-                <div class="mr-5">
-                    <div class="form-group">
-                        <label for="name">Nama Customer</label>
-                        <input type="text" name="name_customer" value="{{ $data->name_customer }}"
-                            class="form-control border-primary" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="price">Total Item</label>
-                        <input type="number" name="total_item" class="form-control border-primary"
-                            value="{{ $data->transaction_detail->sum('qty') }}" readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Total Price</label>
-                        <input type="number" name="price_amount" class="form-control border-primary" value="0"
-                            readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Paid</label>
-                        <input type="number" name="paid" class="form-control border-primary" value=""
-                            onchange="updateReturn()">
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Return</label>
-                        <input type="number" name="return" class="form-control border-primary" value="0" readonly>
-                    </div>
-                    <div class="form-group">
-                        <label for="name">Metode Pembayaran</label>
-                        <select name="payment_id" id="payment_id">
-                            <option value="" readonly selected>Pilih Metode Pembayaran</option>
-                            @foreach ($payment as $pay)
-                                <option value="{{ $pay->id }}">
-                                    {{ $pay->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="row py-2">
-                        <div class="col-8 text-center">
-                            <button type="submit" class="btn btn-lg btn-success btn-block" data-toggle="modal"
-                                data-target="#confirmOrderCenter">
-                                Selesaikan Orderan
-                            </button>
+                    <div class="mr-5">
+                        <div class="form-group">
+                            <label for="name">Nama Customer</label>
+                            <input type="text" name="name_customer" value="{{ $data->name_customer }}"
+                                class="form-control border-primary" required>
                         </div>
+
+                        <div class="form-group">
+                            <label for="price">Total Item</label>
+                            <input type="number" name="total_item" class="form-control border-primary"
+                                value="{{ $data->transaction_detail->sum('qty') }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="price">Total Price</label>
+                            <input type="number" name="price_amount" class="form-control border-primary" value="0"
+                                readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="price">Paid</label>
+                            <input type="number" name="paid" id="paid" class="form-control border-primary"
+                                value="" onchange="updateReturn()">
+                        </div>
+                        <div class="form-group">
+                            <label for="price">Return</label>
+                            <input type="number" name="return" class="form-control border-primary" value="0"
+                                readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Metode Pembayaran</label>
+                            <select name="payment_id" id="payment_id">
+                                <option value="" readonly selected>Pilih Metode Pembayaran</option>
+                                @foreach ($payment as $pay)
+                                    <option value="{{ $pay->id }}">
+                                        {{ $pay->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="row py-2">
+                            <div class="col-8 text-center">
+                                <button type="submit" class="btn btn-lg btn-success btn-block" data-toggle="modal"
+                                    data-target="#confirmOrderCenter" disabled id="btn-selesai">
+                                    Selesaikan Orderan
+                                </button>
+                            </div>
+                </form>
+                <div class="col-4">
+                    <form action="{{ route('transaksi.nota', $data->id) }}" method="GET">
+                        <input type="hidden" name="paid2" value="" id="paid2">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block">Invoice</button>
                     </form>
-                        <div class="col-4">
-                            <form action="{{ route('transaksi.nota', $data->id) }}" method="GET">
-                                <input type="hidden" name="paid2" value="" id="paid2">
-                                <button type="submit" class="btn btn-lg btn-primary btn-block">Invoice</button>
-                            </form>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+    </div>
+    </div>
     <script>
-         function updateSubtotal(input) {
+        function updateSubtotal(input) {
             var price = parseFloat(input.getAttribute('data-price'));
             var quantity = parseInt(input.value);
             var subtotal = price * quantity;
@@ -131,10 +133,40 @@
         }
 
         document.addEventListener("DOMContentLoaded", function() {
-        var inputProduk = document.querySelectorAll('input[name="product[]"]');
-        inputProduk.forEach(function(input) {
-            updateSubtotal(input);
+            var inputProduk = document.querySelectorAll('input[name="product[]"]');
+            inputProduk.forEach(function(input) {
+                updateSubtotal(input);
+            });
         });
-    });
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#paid').on('change', function() {
+                var paid = $(this).val();
+
+                let btn_selesai = document.getElementById("btn-selesai");
+
+                if (paid) {
+                    btn_selesai.disabled = false;
+                } else {
+                    btn_selesai.disabled = true;
+                }
+            });
+
+            // Menangkap perubahan pada input #paid
+            $('#paid').on('input', function() {
+                var paid = $(this).val();
+
+                let btn_selesai = document.getElementById("btn-selesai");
+
+                // Memeriksa apakah nilai paid kosong
+                if (!paid) {
+                    // Menonaktifkan tombol
+                    btn_selesai.disabled = true;
+                }
+            });
+        });
     </script>
 @endsection
