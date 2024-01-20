81b84583086598b6172d4929730ffef4851b29b3
ADITYA IMAM ZUHDI <20102217@ittelkom-pwt.ac.id>
2024-01-20 18:36:45 +0700

    Update index.blade.php

--- resources/views/outlet/pesanan/index.blade.php
+++ resources/views/outlet/pesanan/index.blade.php
@@ -43,14 +43,14 @@
                         <div class="card-body">
                             <h4 class="card-title">List Of Pesanan</h4>
                             <div class="table-responsive">
-                                <table id="zero_config" class="table table-striped table-bordered no-wrap">
+                                <table id="" class="table table-striped table-bordered no-wrap">
                                     <thead>
                                         <tr>
                                             <th>No</th>
-                                            <th>Name</th>
-                                            <th>Table</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
+                                            <th>Nama Pelanggan</th>
+                                            <th>No Meja</th>
+                                            <th>Pesanan</th>
+                                            <th>Selesaikan Pesanan</th>
                                         </tr>
                                     </thead>
                                     <tbody id="content-table">
@@ -71,8 +71,8 @@
                                                 <td>
                                                     <a href="" data-toggle="modal"
                                                         data-target="#modal-edit{{ $item->id }}" style="width: 50px"
-                                                        class="btn btn-warning"><i class="bi bi-pencil"><span
-                                                                class="fas fa-edit"></span></i></a>
+                                                        class="btn btn-success"><i class="bi bi-pencil"><span
+                                                                class="fas fa-check"></span></i></a>
                                                 </td>
                                             </tr>
                                             <div id="modal-edit{{ $item->id }}" class="modal fade" tabindex="-1"
@@ -80,52 +80,46 @@
                                                 <div class="modal-dialog">
                                                     <div class="modal-content">
                                                         <div class="modal-header modal-colored-header bg-primary">
-                                                            <h4 class="modal-title" id="modal-editLabel">Detail Pesanan
+                                                            <h4 class="modal-title" id="modal-editLabel">Selesaikan Pesanan
                                                             </h4>
-                                                            <button type="reset" class="close" data-bs-dismiss="modal"
-                                                                aria-hidden="true">×</button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            <div class="col-sm-12 col-md-12 col-lg-12">
-                                                                <div class="card">
-                                                                    <div class="card-body">
-                                                                        <h4 class="card-title">Pesanan</h4>
-                                                                        <p class="">Nama : {{ $item->name_customer }}</p>
-                                                                        <p class="">No Meja : {{ $item->table->number }}</p>
-                                                                        <form method="POST"
-                                                                            action="{{ route('pesanan.update', $item->id) }}"
-                                                                            enctype="multipart/form-transaction"
-                                                                            class="mt-4">
-                                                                            @method('PUT')
-                                                                            @csrf
-                                                                            @foreach (\App\Models\Transaction_detail::where('transaction_id', $item->id)->get() as $trans)
-                                                                            <div class="row">
-                                                                                <div class="col-10">
-                                                                                    <div class="form-check">
-                                                                                        <input {{ ($trans->status == "Jadi") ? 'checked disabled' : '' }} class="form-check-input" type="checkbox" value="{{ $trans->product_id }}" id="flexCheckDefault" name="produk[]">
-                                                                                        <label class="form-check-label" for="flexCheckDefault">
-                                                                                          {{ \App\Models\Produk::where('id', $trans->product_id)->first()->name }}
-                                                                                        </label>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="col">
-                                                                                    <p>x{{ $trans->qty }}</p>
-                                                                                </div>
-                                                                            </div>
-                                                                            @endforeach
-                                                                            <div class="modal-footer">
-                                                                                <button type="submiy"
-                                                                                    class="btn btn-primary">Tambahkan</button>
-                                                                            </div>
-                                                                        </form>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
+                                                            <h4 class="px-5 text-center mb-5"><strong>Apakah Akan Menyelesaikan Pesanan?</strong></h4>
+                                                            <h1 class="px-5 text-center text-primary "><span class="fas fa-check-circle"></span></strong></h4>
+                                                            
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                          <button type="button" class="btn btn-secondary ms-auto" data-bs-dismiss="modal">Batal</button>
+                                                          <form action="" method="POST">
+                                                            @method('PUT')
+                                                            @csrf
+                                                            <button class="btn btn-primary" type="submit">Lanjutkan</button>
+                                                          </form>
                                                         </div>
 
                                                     </div><!-- /.modal-content -->
                                                 </div><!-- /.modal-dialog -->
                                             </div><!-- /.modal -->
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="table-responsive">
+                                <table id="zero_config" class="table table-striped table-bordered no-wrap">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Pelanggan</th>
+                                            <th>No Meja</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="content-table">
+                                        @foreach ($transaksi as $item)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $item->name_customer }}</td>
+                                                <td>{{ $item->table->number }}</td>
+                                            </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
