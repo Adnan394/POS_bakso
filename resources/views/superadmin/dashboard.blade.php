ca60732599fb834a54b9de5122fb8416ea4ca345
ADITYA IMAM ZUHDI <20102217@ittelkom-pwt.ac.id>
2024-02-17 15:54:10 +0700

    Update dashboard.blade.php

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -57,7 +57,7 @@
                         <div>
                             <h2 class="text-dark mb-1 w-100 text-truncate font-weight-medium"><sup
                                     class="set-doller">Rp.</sup>{{ $revenue_week }}</h2>
-                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Pendapatan Bulan ini
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Pendapatan Minggu ini
                             </h6>
                         </div>
                         <div class="ml-auto mt-md-3 mt-lg-0">
@@ -71,25 +71,12 @@
                     <div class="d-flex d-lg-flex d-md-block align-items-center">
                         <div>
                             <div class="d-inline-flex align-items-center">
-                                <h2 class="text-dark mb-1 font-weight-medium">{{ $revenue_week }}</h2>
+                                <h2 class="text-dark mb-1 font-weight-medium">{{ $revenue_month }}</h2>
                             </div>
-                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Akun Cabang</h6>
+                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Pendapatan Bulan ini</h6>
                         </div>
                         <div class="ml-auto mt-md-3 mt-lg-0">
-                            <span class="opacity-7 text-muted"><i data-feather="file-plus"></i></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex d-lg-flex d-md-block align-items-center">
-                        <div>
-                            <h2 class="text-dark mb-1 font-weight-medium">864</h2>
-                            <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Projects</h6>
-                        </div>
-                        <div class="ml-auto mt-md-3 mt-lg-0">
-                            <span class="opacity-7 text-muted"><i data-feather="globe"></i></span>
+                            <span class="opacity-7 text-muted"><i data-feather="dollar-sign"></i></span>
                         </div>
                     </div>
                 </div>
