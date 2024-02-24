57605f3e88bef07e93b64b82722fe92ae6361248
ADITYA IMAM ZUHDI <20102217@ittelkom-pwt.ac.id>
2024-02-24 22:28:01 +0700

    Update nota-dapur.blade.php

--- resources/views/kasir/transaksi/nota-dapur.blade.php
+++ resources/views/kasir/transaksi/nota-dapur.blade.php
@@ -9,45 +9,45 @@
 
     <?php
     $style = '
-        <style>
-            * {
-                font-family: "consolas", sans-serif;
-            }
-            p {
-                display: block;
-                margin: 3px;
-                font-size: 15pt;
-            }
-            table td {
-                font-size: 12pt;
-            }
-            .text-center {
-                text-align: center;
-            }
-            .text-right {
-                text-align: right;
-            }
-    
-            @media print {
-                @page {
-                    margin: 0;
-                    size: 75mm 
-        ';
+                                                        <style>
+                                                            * {
+                                                                font-family: "consolas", sans-serif;
+                                                            }
+                                                            p {
+                                                                display: block;
+                                                                margin: 3px;
+                                                                font-size: 15pt;
+                                                            }
+                                                            table td {
+                                                                font-size: 12pt;
+                                                            }
+                                                            .text-center {
+                                                                text-align: center;
+                                                            }
+                                                            .text-right {
+                                                                text-align: right;
+                                                            }
+                                                    
+                                                            @media print {
+                                                                @page {
+                                                                    margin: 0;
+                                                                    size: 75mm 
+                                                        ';
     ?>
     <?php
     $style .= !empty($_COOKIE['innerHeight']) ? $_COOKIE['innerHeight'] . 'mm; }' : '}';
     ?>
     <?php
     $style .= '
-                html, body {
-                    width: 70mm;
-                }
-                .btn-print {
-                    display: none;
-                }
-            }
-        </style>
-        ';
+                                                                html, body {
+                                                                    width: 70mm;
+                                                                }
+                                                                .btn-print {
+                                                                    display: none;
+                                                                }
+                                                            }
+                                                        </style>
+                                                        ';
     ?>
 
     {!! $style !!}
@@ -61,8 +61,7 @@
     </div>
     <br>
     <div>
-        <p style="float: left;">{{ date('d-m-Y') }}</p>
-        <p style="float: right">{{ strtoupper(auth()->user()->name) }}</p>
+        <p style="float: left">{{ strtoupper(auth()->user()->name) }}</p>
     </div>
     <div class="clear-both" style="clear: both;"></div>
     {{-- <p>No: {{ ($data->id) }}</p> --}}
@@ -72,7 +71,10 @@
             Nama : {{ $product[0]->name_customer }}
         </p>
         <p>
-            Meja : {{ ($product[0]->table->number) ?? "" }}
+            Meja : {{ $product[0]->table->number ?? '' }}
+        </p>
+        <p>
+            Waktu Transaksi : {{ $product[0]->created_at }}
         </p>
     </div>
     <p class="text-center">===================================</p>
@@ -86,39 +88,12 @@
                 <td colspan="3">{{ ($item->table->number) ?? "" }}</td>
             </tr> --}}
             <tr>
-                <td>{{ $item->qty }} x {{ number_format($item->price, '0', ',', '.') }}</td>
-                <td class="text-right">{{ number_format($item->qty * $item->price, '0', ',', '.') }}</td>
+                <td>{{ $item->qty }} {{ $item->order_type }}</td>
+                <td class="text-right"> {{ $item->note != null ? "pesan: " . $item->note : "" }}</td>
             </tr>
         @endforeach
     </table>
-    <p class="text-center">-----------------------------------</p>
 
-    <table width="100%" style="border: 0;">
-        <tr>
-            <td>Total Harga:</td>
-            <td class="text-right">{{ number_format($data->price_amount, '0', ',', '.') }}</td>
-        </tr>
-        <tr>
-            <td>Total Item:</td>
-            <td class="text-right">{{ number_format($product->sum('qty'), '0', ',', '.') }}</td>
-        </tr>
-        <tr>
-            <td>Diskon:</td>
-            <td class="text-right">{{ $data->discount }}</td>
-        </tr>
-        <tr>
-            <td>Total Harga:</td>
-            <td class="text-right">{{ number_format($data->price_amount, '0', ',', '.') }}</td>
-        </tr>
-        <tr>
-            <td>Jumlah Bayar:</td>
-            <td class="text-right">{{ number_format($product[0]->pay_receive, '0', ',', '.') }}</td>
-        </tr>
-        <tr>
-            <td>Kembali:</td>
-            <td class="text-right">{{ $product[0]->pay_return }}</td>
-        </tr>
-    </table>
 
     <p class="text-center">===================================</p>
     <p class="text-center">-- TERIMA KASIH --</p>
@@ -151,12 +126,12 @@
                 },
                 jsPDF: {
                     unit: 'mm',
-                    format: 'a6',
+                    format: 'a5',
                     orientation: 'portrait'
                 }
             });
 
-            // Menambahkan penundaan 3 detik sebelum mengalihkan
+            Menambahkan penundaan 3 detik sebelum mengalihkan
             setTimeout(function() {
                 window.location.href = "{{ route('pesanan_selesai') }}";
             }, 5000);
